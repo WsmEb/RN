e513a1d0430fcbc07984a7554dd216bb2d795a62
Wsm404 <wassimelbachiri38@gmail.com>
2024-07-21 16:52:47 +0100

    Push the project to gitHub -1

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Everything is good now :)</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text : {
+    color : "white",
+    fontSize : 17,
+    fontWeight : "bold",
+    marginBottom : 20
+  }
+});
